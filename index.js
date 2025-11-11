71206c23a1106a5332bd9e29bfd33cf438b06103
M Eran Hasareli <eran.hasarali.2001@gmail.com>
2025-11-11 19:36:04 +0530

    connect to the mongodb

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
+import connectDB from "./configs/db.js";
 
 import userRegisterRoutes from "./routes/auth/register/userRegisterRoutes.js";
 
 dotenv.config();
+
+connectDB();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
